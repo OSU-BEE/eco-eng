c5b7ad525c9d4f8dcdd743a3af800bec07bb583a
boltej <john.bolte@oregonstate.edu>
2019-12-23 21:19:29 -0800

    restructure github site

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(name='eco-eng',
+version='0.1',
+description='Tools for Ecological Engineering',
+url='https://github.com/OSU-BEE/eco-eng',
+author='boltej',
+author_email='john.bolte@oregonstate.edu',
+license='MIT',
+packages=['eco-eng'],
+zip_safe=False)
